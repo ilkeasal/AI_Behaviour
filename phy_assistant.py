c303ddcd8f697b88ad52bee713498e6b20868e83
ilkeasal <125877580+ilkeasal@users.noreply.github.com>
2024-12-22 11:40:09 +0100

    Update phy_assistant.py

--- phy_assistant.py
+++ phy_assistant.py
@@ -437,7 +437,7 @@ def stop_or_continue(a_dict):
 
 def submit_submit_function():
     st.session_state.start_experiment = True
-    st.session_state.log_buffer.write("PHY ASSESSMENT!")
+    st.session_state.log_buffer.write("PHY ASSESSMENT!\n")
     st.session_state.log_buffer.write(f"Days input : {days_input}\n")
     st.session_state.log_buffer.write(f"Hours input : {hours_input}\n")
     st.session_state.log_buffer.write(f"Strength input : {strength_input}\n")
