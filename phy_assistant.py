e7f0a0e8e384b58c33b3b2dce61db2ed2d20d55d
ilkeasal <125877580+ilkeasal@users.noreply.github.com>
2025-01-06 05:55:43 +0100

    Update phy_assistant.py

--- phy_assistant.py
+++ phy_assistant.py
@@ -503,7 +503,7 @@ if "start_experiment" not in st.session_state:
 if not st.session_state.start_experiment:
     #st.write("How often do you use chatbots?")
     age = st.number_input("What is your age?",min_value=18, max_value=64)
-    st.session_state.log_buffer.write(f"User age : {age}")
+    st.session_state.log_buffer.write(f"User age : {age}\n")
     options = {
         1:"Never",
         2:"1-2 times a year",
