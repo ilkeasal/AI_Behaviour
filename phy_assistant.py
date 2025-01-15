13714bf7d37f923dc1440f6aaf2e9365560bce5b
ilkeasal <125877580+ilkeasal@users.noreply.github.com>
2025-01-15 15:08:40 +0100

    Update phy_assistant.py

--- phy_assistant.py
+++ phy_assistant.py
@@ -434,7 +434,7 @@ def stop_button():
     st.session_state.log_buffer.write("STOP BUTTON PRESSED!\n")
     if st.session_state.advice_given == False:
         advice_because_no_advice()
-        #st.session_state.start_experiment = "post-survey"
+        st.session_state.start_experiment = "post-survey"
     else :
         st.session_state.start_experiment = "post-survey"
 
