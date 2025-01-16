0e085b8cab1369ad7a20f936146a2c562436c7e7
ilkeasal <125877580+ilkeasal@users.noreply.github.com>
2025-01-16 15:54:01 +0100

    Update phy_assistant.py

--- phy_assistant.py
+++ phy_assistant.py
@@ -583,7 +583,7 @@ if st.session_state.start_experiment=="pre-survey":
         4:"1-2 times a week",
         5:"Daily"
     }
-    chatbot_use= st.radio("How often do you use chatbots? ", options.keys(),index=None,format_func=lambda x:options[x])
+    chatbot_use= st.radio("How often do you use chatbots?(For example, AI-based chatbots like ChatGPT that can engage in a conversation with you.)", options.keys(),index=None,format_func=lambda x:options[x])
     #st.session_state.log_buffer.write(f"Chatbot user experience : {response} \n")
     days_input = st.number_input("On average, how many days per week do you engage in moderate to strenuous exercise?", min_value=0,max_value=7)
     hours_input = st.number_input("On average, how many minutes do you engage in exercise at this level?",min_value=0,max_value=240)
