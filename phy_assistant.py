c68364c55b2e4cc535201bbfdb93af6af8bcf6ce
ilkeasal <125877580+ilkeasal@users.noreply.github.com>
2025-01-27 10:58:19 +0100

    Update phy_assistant.py

--- phy_assistant.py
+++ phy_assistant.py
@@ -624,7 +624,7 @@ if "all_known_concepts" not in st.session_state:
 if "experiment_condition" not in st.session_state: #### TEMPORARY EXPERIMENT CONDITION , 1 indicates experiment, 2 indicates control
    st.session_state.experiment_condition = 1
    st.session_state.log_buffer.write("Experiment Condition :")
-   st.session_state.log_buffer.write("PILOT STUDY")
+   st.session_state.log_buffer.write("PILOT STUDY\n")
    #st.session_state.log_buffer.write("Control Condition : 2")
       
 
