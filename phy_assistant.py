52856bcd53cd98f5651c8b03434e3f9838d6ad04
ilkeasal <125877580+ilkeasal@users.noreply.github.com>
2025-02-10 09:51:02 +0100

    Update phy_assistant.py

--- phy_assistant.py
+++ phy_assistant.py
@@ -1427,8 +1427,9 @@ if st.session_state.experiment_condition == 1:
                              st.session_state.users_level = initialize_users_level_dict(st.session_state.unstable_concept_dict)
                              print(f"The users level dictionary : {st.session_state.users_level}")
                              st.session_state.problematic_sentence = chat_history[-1]
-                             st.session_state.log_buffer.write(f"Problematic sentence : {st.session_state.problematic_sentence}")
-                             st.write(st.session_state.problematic_sentence)
+                             st.session_state.log_buffer.write(f"Problematic sentence : {st.session_state.problematic_sentence}\n")
+                             st.session_state.log_buffer.write("\n")
+                             #st.write(st.session_state.problematic_sentence)
                              st.session_state.log_buffer.write(f"USERS LEVEL : {st.session_state.users_level}\n")
                              # Now ask a question about the first key in that dictionary.
                              st.session_state.last_asked_key = next(iter(st.session_state.unstable_concept_dict))
